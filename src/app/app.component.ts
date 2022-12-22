a9a305817622725bedfa89f65c90ec5aa91edd45
Pedro Angel <pavg_92@hotmail.com>
2022-12-22 11:54:43 -0600

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  title = 'Uso de pipes de Angular';
+  nombre = 'Pedro angel Velazquez garCia';
+  saldo = 123.345;
+  hoy = new Date();
+
+  cursos = [
+    {nombre: 'HTML5', dias: 5 , calificacion: 90},
+    {nombre: 'javaScript', dias: 10, calificacion: 100},
+    {nombre: 'CSS3', dias: 3, calificacion: 100}
+  ]
+
 }
